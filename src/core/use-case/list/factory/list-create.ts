62523f47c49aea108ab910858ba3cd13839b3a6c
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2022-11-01 17:41:18 -0300

    Instantiate the ListCreateGateway in factoryListCreate

--- src/core/use-case/list/factory/list-create.ts
+++ src/core/use-case/list/factory/list-create.ts
@@ -1,8 +1,10 @@
+import { ListCreateGateway } from '@app/gateway/list-gateway'
 import { ListRepositoryMemory } from '@infra/repositories/memory/list-repository'
 import { ListCreateUseCase } from '../list-create'
 import { listCreateMock } from '../mock/list-create-mock'
 
 export function factoryListCreate (): ListCreateUseCase {
   const repository = new ListRepositoryMemory([listCreateMock])
-  return new ListCreateUseCase(repository)
+  const gateway = new ListCreateGateway()
+  return new ListCreateUseCase(repository, gateway)
 }
