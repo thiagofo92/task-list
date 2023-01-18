a431eded5471e1bb2b97a1e897e891ecf6838b03
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2023-01-18 16:39:12 -0300

    Start to create the method creation

--- src/app/usecase/login-use-case.ts
+++ src/app/usecase/login-use-case.ts
@@ -1,6 +1,11 @@
+import { LoginCreationInModel } from '@app/model'
 import { LoginRepository } from '@core/repositories'
 import { LoginUseCaseContract } from '@core/usecase'
 
 export class LoginUseCase implements LoginUseCaseContract {
   constructor (private readonly loginService: LoginRepository) {}
+
+  async create (login: LoginCreationInModel): Promise<unknown> {
+    return ''
+  }
 }
