dfad3a8638fbd1e40d51ef66fbd3d6aa10fd9526
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2023-01-18 16:12:23 -0300

    Add update method

--- src/core/repositories/login-repository.ts
+++ src/core/repositories/login-repository.ts
@@ -1,9 +1,17 @@
-import { LoginAuthenticationInModel, LoginCreationInModel, LoginCreationOutModel, LoginFetchOutModel } from '@app/model'
+import {
+  LoginAuthenticationInModel,
+  LoginCreationInModel,
+  LoginCreationOutModel,
+  LoginFetchOutModel,
+  LoginUpdateInModel,
+  LoginUpdateOutModel
+} from '@app/model'
 
 export interface LoginRepository {
   create: (login: LoginCreationInModel) => Promise<LoginCreationOutModel>
+  update: (login: LoginUpdateInModel) => Promise<LoginUpdateOutModel | null>
   findAll: () => Promise<LoginFetchOutModel[]>
   findById: (id: string) => Promise<LoginFetchOutModel | null>
-  findByEmail: (email: string) => Promise<LoginFetchOutModel>
+  findByEmail: (email: string) => Promise<LoginFetchOutModel | null>
   findByLoginAndPassword: (login: LoginAuthenticationInModel) => Promise<boolean>
 }
