ef0fca9ff052318aec4111232997b5f5602d2050
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2023-01-18 16:38:37 -0300

    Start test with creation method

--- src/app/usecase/__test__/login-use-case.spec.ts
+++ src/app/usecase/__test__/login-use-case.spec.ts
@@ -1,18 +1,29 @@
 import { LoginRepository } from '@core/repositories'
 import { LoginMockService } from '@infra/services/__test__/__mocks__/login-service-mock'
 import { describe, test, expect, vi } from 'vitest'
+import { LoginUseCase } from '../login-use-case'
+import { LoginCreationInModel } from '@app/model'
 
 interface FactoryLogin {
-  sut: LoginRepository
+  sut: LoginUseCase
 }
 
 function Factory (): FactoryLogin {
-  const sut = new LoginMockService()
+  const service = new LoginMockService()
+  const sut = new LoginUseCase(service)
   return { sut }
 }
 
 describe('# Login Use case', () => {
-  test.todo('Sucess to create login')
+  test('Sucess to create login', async () => {
+    const { sut } = Factory()
+    const login: LoginCreationInModel = {} as any
+
+    const result = await sut.create(login)
+    const expected = ''
+
+    expect(result).toStrictEqual(expected)
+  })
   test.todo('Sucess to update login')
   test.todo('Sucess to authentica the login and password')
   test.todo('Sucess to find all login')
