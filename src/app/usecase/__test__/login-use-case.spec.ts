ade1d79d2aed471e8f6a4373dfb41b48e5083212
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2023-01-23 16:12:30 -0300

    Add test to update method - login usecase

--- src/app/usecase/__test__/login-use-case.spec.ts
+++ src/app/usecase/__test__/login-use-case.spec.ts
@@ -1,7 +1,8 @@
 import { LoginMockService } from '@infra/services/__test__/__mocks__/login-service-mock'
-import { describe, test, expect, vi } from 'vitest'
+import { describe, test, expect } from 'vitest'
 import { LoginUseCase } from '../login-use-case'
 import { LoginCreationInModel, LoginUpdateInModel } from '@app/model/input'
+import { LoginCreationOutModel } from '@app/model/output'
 
 interface FactoryLogin {
   sut: LoginUseCase
@@ -19,18 +20,33 @@ describe('# Login Use case', () => {
     const login: LoginCreationInModel = {} as any
 
     const result = await sut.create(login)
-    const expected = { id: '1' }
+    const expected = { id: '0' }
 
     expect(result.value).toStrictEqual(expected)
   })
 
   test('Sucess to update login', async () => {
     const { sut } = Factory()
-    const login: LoginUpdateInModel = {} as any
+    const login: LoginCreationInModel = {
+      password: 'test',
+      email: 'test@test',
+      nickName: 'test_test'
+    }
 
-    const result = await sut.update(login)
-    const expected = { id: '1' }
+    const created = await sut.create(login)
+    const loginCreated = created.value as LoginCreationOutModel
+    const loginUpdate: LoginUpdateInModel = {
+      id: loginCreated.id,
+      nickName: 'Updated test',
+      password: 'Updated password'
+    }
 
+    const expected = {
+      nickName: loginUpdate.nickName,
+      password: loginUpdate.password
+    }
+
+    const result = await sut.update(loginUpdate)
     expect(result.value).toStrictEqual(expected)
   })
   test.todo('Sucess to authenticate the login and password')
