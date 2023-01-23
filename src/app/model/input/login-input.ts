f4b7bb194b8aa8394cf3f6d8dffd445ca6070802
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2023-01-23 15:14:35 -0300

    Separated the input model and output model in each own folder

---
+++ src/app/model/input/login-input.ts
@@ -0,0 +1,16 @@
+export interface LoginAuthenticationInModel {
+  email: string
+  password: string
+}
+
+export interface LoginCreationInModel {
+  nickName: string
+  email: string
+  password: string
+}
+
+export interface LoginUpdateInModel {
+  id: string
+  nickName: string
+  password: string
+}
---
+++ src/app/model/output/login-output.ts
@@ -0,0 +1,13 @@
+export interface LoginUpdateOutModel {
+  nickName: string
+  password: string
+}
+export interface LoginFetchOutModel {
+  id: string
+  nickName: string
+  email: string
+}
+
+export interface LoginCreationOutModel {
+  id: string
+}
