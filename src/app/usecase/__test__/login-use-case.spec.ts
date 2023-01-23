90bdd414cea1275ffbcb12cc1a5bdc2016b238f6
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2023-01-23 15:15:28 -0300

    Change the path for login model and start the update test

--- src/app/usecase/__test__/login-use-case.spec.ts
+++ src/app/usecase/__test__/login-use-case.spec.ts
@@ -1,8 +1,7 @@
-import { LoginRepository } from '@core/repositories'
 import { LoginMockService } from '@infra/services/__test__/__mocks__/login-service-mock'
 import { describe, test, expect, vi } from 'vitest'
 import { LoginUseCase } from '../login-use-case'
-import { LoginCreationInModel } from '@app/model'
+import { LoginCreationInModel, LoginUpdateInModel } from '@app/model/input'
 
 interface FactoryLogin {
   sut: LoginUseCase
@@ -22,10 +21,18 @@ describe('# Login Use case', () => {
     const result = await sut.create(login)
     const expected = { id: '1' }
 
-    expect(result).toStrictEqual(expected)
+    expect(result.value).toStrictEqual(expected)
   })
 
-  test.todo('Sucess to update login')
+  test('Sucess to update login', async () => {
+    const { sut } = Factory()
+    const login: LoginUpdateInModel = {} as any
+
+    const result = await sut.update(login)
+    const expected = { id: '1' }
+
+    expect(result.value).toStrictEqual(expected)
+  })
   test.todo('Sucess to authenticate the login and password')
   test.todo('Sucess to find all login')
   test.todo('Sucess to find login by id')
