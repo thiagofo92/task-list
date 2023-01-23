603d30b22a7ffb8e2bc45e341dce5b92506f75e8
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2023-01-23 16:15:10 -0300

    Add login update method

--- src/app/usecase/login-use-case.ts
+++ src/app/usecase/login-use-case.ts
@@ -2,12 +2,14 @@ import { LoginCreationInModel, LoginUpdateInModel } from '@app/model/input'
 import { LoginRepository } from '@core/repositories'
 import { LoginUseCaseContract } from '@core/usecase'
 import { Either, left, right } from '@shared/error/etheir'
-import { LoginUseCaseCreationError } from './errors/login-error'
+import { LoginUseCaseCreationError, LoginUseCaseUpdateError } from './errors/login-error'
+import { LoginUpdateError } from '@infra/services/error/login-error'
+import { LoginCreationOutModel } from '@app/model/output'
 
 export class LoginUseCase implements LoginUseCaseContract {
   constructor (private readonly loginService: LoginRepository) {}
 
-  async create (login: LoginCreationInModel): Promise<Either<LoginUseCaseCreationError, unknown>> {
+  async create (login: LoginCreationInModel): Promise<Either<LoginUseCaseCreationError, LoginCreationOutModel>> {
     const successOrError = await this.loginService.create(login)
 
     if (successOrError.isLeft()) return left(new LoginUseCaseCreationError(successOrError.value.message))
@@ -15,10 +17,10 @@ export class LoginUseCase implements LoginUseCaseContract {
     return right(successOrError.value)
   }
 
-  async update (login: LoginUpdateInModel): Promise<Either<LoginUseCaseCreationError, unknown>> {
+  async update (login: LoginUpdateInModel): Promise<Either<LoginUpdateError, unknown>> {
     const successOrError = await this.loginService.update(login)
 
-    if (successOrError.isLeft()) return left(new LoginUseCaseCreationError(successOrError.value.message))
+    if (successOrError.isLeft()) return left(new LoginUseCaseUpdateError(successOrError.value.message))
 
     return right(successOrError.value)
   }
