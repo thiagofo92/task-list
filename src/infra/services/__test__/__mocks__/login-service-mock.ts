d2fd140b44abca98532397e627d77eab3f91aede
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2023-01-23 14:46:57 -0300

    Change type of return to Either

--- src/infra/services/__test__/__mocks__/login-service-mock.ts
+++ src/infra/services/__test__/__mocks__/login-service-mock.ts
@@ -6,11 +6,20 @@ import {
   LoginUpdateInModel,
   LoginUpdateOutModel
 } from '@app/model'
+import {
+  LoginCreationError,
+  LoginFinByLoginAndPasswordError,
+  LoginFindAllError,
+  LoginFindByEmailError,
+  LoginFindByIdError,
+  LoginUpdateError
+} from '@app/usecase/error/login-error'
 import { LoginRepository } from '@core/repositories/'
+import { Either, right } from '@shared/error/etheir'
 
 export class LoginMockService implements LoginRepository {
   private readonly loginMock: Array<{ id: string } & LoginCreationInModel> = []
-  async create (login: LoginCreationInModel): Promise<LoginCreationOutModel> {
+  async create (login: LoginCreationInModel): Promise<Either<LoginCreationError, LoginCreationOutModel>> {
     this.loginMock.push({
       id: this.loginMock.length.toString(),
       ...login
@@ -19,24 +28,24 @@ export class LoginMockService implements LoginRepository {
       id: this.loginMock.length.toString()
     }
 
-    return result
+    return right(result)
   }
 
-  async update (login: LoginUpdateInModel): Promise<LoginUpdateOutModel | null> {
+  async update (login: LoginUpdateInModel): Promise<Either<LoginUpdateError, LoginUpdateOutModel | null>> {
     const index = this.loginMock.findIndex(item => item.id === login.id)
 
-    if (!index) return null
+    if (!index) return right(null)
 
     this.loginMock[index].nickName = login.nickName
     this.loginMock[index].password = login.password
 
-    return {
+    return right({
       nickName: this.loginMock[index].nickName,
       password: this.loginMock[index].password
-    }
+    })
   }
 
-  async findAll (): Promise<LoginFetchOutModel[]> {
+  async findAll (): Promise<Either<LoginFindAllError, LoginFetchOutModel[]>> {
     const result = this.loginMock.map<LoginFetchOutModel>((value, index) => (
       {
         id: index.toString(),
@@ -45,31 +54,31 @@ export class LoginMockService implements LoginRepository {
       }
     ))
 
-    return result
+    return right(result)
   }
 
-  async findById (id: string): Promise<LoginFetchOutModel | null> {
+  async findById (id: string): Promise<Either<LoginFindByIdError, LoginFetchOutModel | null>> {
     const result = this.loginMock.find(item => item.id === id)
 
-    if (!result) return null
+    if (!result) return right(null)
 
-    return {
+    return right({
       id: result?.id,
       email: result?.email,
       nickName: result?.nickName
-    }
+    })
   }
 
-  async findByEmail (email: string): Promise<LoginFetchOutModel | null> {
+  async findByEmail (email: string): Promise<Either<LoginFindByEmailError, LoginFetchOutModel | null>> {
     const result = this.loginMock.find(item => item.email === email)
 
-    if (!result) return null
+    if (!result) return right(null)
 
-    return result
+    return right(result)
   }
 
-  async findByLoginAndPassword (login: LoginAuthenticationInModel): Promise<boolean> {
+  async findByLoginAndPassword (login: LoginAuthenticationInModel): Promise<Either<LoginFinByLoginAndPasswordError, boolean>> {
     const result = this.loginMock.find(item => item.email === login.email && item.password === login.password)
-    return result !== null
+    return right(result !== null)
   }
 }
