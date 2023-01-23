f3daeb0d7aa3a94c0871eb9c8f39b78a0bb6da5f
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2023-01-23 16:39:34 -0300

    Created the method findById

--- src/core/usecase/login-use-case.ts
+++ src/core/usecase/login-use-case.ts
@@ -1,10 +1,16 @@
 import { LoginCreationInModel, LoginUpdateInModel } from '@app/model/input'
 import { LoginCreationOutModel } from '@app/model/output'
-import { LoginUseCaseCreationError, LoginUseCaseFindAllError, LoginUseCaseUpdateError } from '@app/usecase/errors/login-error'
+import {
+  LoginUseCaseCreationError,
+  LoginUseCaseFindAllError,
+  LoginUseCaseFindByIdError,
+  LoginUseCaseUpdateError
+} from '@app/usecase/errors/login-error'
 import { Either } from '@shared/error/etheir'
 
 export interface LoginUseCaseContract {
   create: (login: LoginCreationInModel) => Promise<Either<LoginUseCaseCreationError, LoginCreationOutModel>>
   update: (login: LoginUpdateInModel) => Promise<Either<LoginUseCaseUpdateError, unknown>>
   findAll: () => Promise<Either<LoginUseCaseFindAllError, unknown>>
+  findById: (id: string) => Promise<Either<LoginUseCaseFindByIdError, unknown>>
 }
