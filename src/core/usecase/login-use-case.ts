1da532460c2bf74f773151a02a4d34f83a1f1531
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2023-01-23 15:17:03 -0300

    Change the path to login model

--- src/core/usecase/login-use-case.ts
+++ src/core/usecase/login-use-case.ts
@@ -1,5 +1,7 @@
-import { LoginCreationInModel } from '@app/model'
+import { LoginCreationInModel } from '@app/model/input'
+import { LoginUseCaseCreationError } from '@app/usecase/errors/login-error'
+import { Either } from '@shared/error/etheir'
 
 export interface LoginUseCaseContract {
-  create: (login: LoginCreationInModel) => Promise<unknown>
+  create: (login: LoginCreationInModel) => Promise<Either<LoginUseCaseCreationError, unknown>>
 }
