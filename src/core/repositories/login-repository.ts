1d52887b2067a243c310cad0de9053fbf56e98e7
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2023-01-23 14:46:39 -0300

    Change type of return to Either

--- src/core/repositories/login-repository.ts
+++ src/core/repositories/login-repository.ts
@@ -6,12 +6,21 @@ import {
   LoginUpdateInModel,
   LoginUpdateOutModel
 } from '@app/model'
+import {
+  LoginCreationError,
+  LoginFinByLoginAndPasswordError,
+  LoginFindAllError,
+  LoginFindByEmailError,
+  LoginFindByIdError,
+  LoginUpdateError
+} from '@app/usecase/error/login-error'
+import { Either } from '@shared/error/etheir'
 
 export interface LoginRepository {
-  create: (login: LoginCreationInModel) => Promise<LoginCreationOutModel>
-  update: (login: LoginUpdateInModel) => Promise<LoginUpdateOutModel | null>
-  findAll: () => Promise<LoginFetchOutModel[]>
-  findById: (id: string) => Promise<LoginFetchOutModel | null>
-  findByEmail: (email: string) => Promise<LoginFetchOutModel | null>
-  findByLoginAndPassword: (login: LoginAuthenticationInModel) => Promise<boolean>
+  create: (login: LoginCreationInModel) => Promise<Either<LoginCreationError, LoginCreationOutModel>>
+  update: (login: LoginUpdateInModel) => Promise<Either<LoginUpdateError, LoginUpdateOutModel | null>>
+  findAll: () => Promise<Either<LoginFindAllError, LoginFetchOutModel[]>>
+  findById: (id: string) => Promise<Either<LoginFindByIdError, LoginFetchOutModel | null>>
+  findByEmail: (email: string) => Promise<Either<LoginFindByEmailError, LoginFetchOutModel | null>>
+  findByLoginAndPassword: (login: LoginAuthenticationInModel) => Promise<Either<LoginFinByLoginAndPasswordError, boolean>>
 }
