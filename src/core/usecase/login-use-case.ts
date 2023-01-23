ef960abfd7441cb6a29ad7e0f6d46fae2940b201
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2023-01-23 16:26:03 -0300

    Create the method findall with test

--- src/core/usecase/login-use-case.ts
+++ src/core/usecase/login-use-case.ts
@@ -1,9 +1,10 @@
 import { LoginCreationInModel, LoginUpdateInModel } from '@app/model/input'
 import { LoginCreationOutModel } from '@app/model/output'
-import { LoginUseCaseCreationError, LoginUseCaseUpdateError } from '@app/usecase/errors/login-error'
+import { LoginUseCaseCreationError, LoginUseCaseFindAllError, LoginUseCaseUpdateError } from '@app/usecase/errors/login-error'
 import { Either } from '@shared/error/etheir'
 
 export interface LoginUseCaseContract {
   create: (login: LoginCreationInModel) => Promise<Either<LoginUseCaseCreationError, LoginCreationOutModel>>
   update: (login: LoginUpdateInModel) => Promise<Either<LoginUseCaseUpdateError, unknown>>
+  findAll: () => Promise<Either<LoginUseCaseFindAllError, unknown>>
 }
