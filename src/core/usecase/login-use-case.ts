a4e1a6b9113c5c82ae7869aa63fd87784124ef18
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2023-01-24 18:20:12 -0300

    Change path and name for login repository

--- src/core/usecase/login-use-case.ts
+++ src/core/usecase/login-use-case.ts
@@ -1,8 +1,9 @@
-import { LoginCreationInModel, LoginUpdateInModel } from '@app/model/input'
+import { LoginAuthenticationInModel, LoginCreationInModel, LoginUpdateInModel } from '@app/model/input'
 import { LoginCreationOutModel } from '@app/model/output'
 import {
   LoginUseCaseCreationError,
   LoginUseCaseFindAllError,
+  LoginUseCaseFindByEmailError,
   LoginUseCaseFindByIdError,
   LoginUseCaseUpdateError
 } from '@app/usecase/errors/login-error'
@@ -13,4 +14,6 @@ export interface LoginUseCaseContract {
   update: (login: LoginUpdateInModel) => Promise<Either<LoginUseCaseUpdateError, unknown>>
   findAll: () => Promise<Either<LoginUseCaseFindAllError, unknown>>
   findById: (id: string) => Promise<Either<LoginUseCaseFindByIdError, unknown>>
+  findByEmail: (email: string) => Promise<Either<LoginUseCaseFindByEmailError, unknown>>
+  authenticationLogin: (login: LoginAuthenticationInModel) => Promise<Either<LoginUseCaseFindByEmailError, boolean>>
 }
