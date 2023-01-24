37b4ef77d81630cc46335af60897d9690ce2e7b5
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2023-01-24 18:18:38 -0300

    Change path and name for login repository

--- src/app/usecase/login-use-case.ts
+++ src/app/usecase/login-use-case.ts
@@ -1,16 +1,26 @@
-import { LoginCreationInModel, LoginUpdateInModel } from '@app/model/input'
-import { LoginRepository } from '@core/repositories'
+import { LoginAuthenticationInModel, LoginCreationInModel, LoginUpdateInModel } from '@app/model/input'
+import { LoginContractRepository } from '@core/repositories'
 import { LoginUseCaseContract } from '@core/usecase'
 import { Either, left, right } from '@shared/error/etheir'
-import { LoginUseCaseCreationError, LoginUseCaseFindByIdError, LoginUseCaseUpdateError } from './errors/login-error'
-import { LoginFindAllError, LoginFindByIdError, LoginUpdateError } from '@infra/services/error/login-error'
+import {
+  LoginUseCaseCreationError,
+  LoginUseCaseFindByEmailError,
+  LoginUseCaseFindByIdError,
+  LoginUseCaseUpdateError
+} from './errors/login-error'
+import {
+  LoginFindAllError,
+  LoginFindByEmailError,
+  LoginFindByIdError,
+  LoginUpdateError
+} from '@infra/repositories/error/login-error'
 import { LoginCreationOutModel } from '@app/model/output'
 
 export class LoginUseCase implements LoginUseCaseContract {
-  constructor (private readonly loginService: LoginRepository) {}
+  constructor (private readonly loginRepository: LoginContractRepository) {}
 
   async create (login: LoginCreationInModel): Promise<Either<LoginUseCaseCreationError, LoginCreationOutModel>> {
-    const successOrError = await this.loginService.create(login)
+    const successOrError = await this.loginRepository.create(login)
 
     if (successOrError.isLeft()) return left(new LoginUseCaseCreationError(successOrError.value.message))
 
@@ -18,7 +28,7 @@ export class LoginUseCase implements LoginUseCaseContract {
   }
 
   async update (login: LoginUpdateInModel): Promise<Either<LoginUpdateError, unknown>> {
-    const successOrError = await this.loginService.update(login)
+    const successOrError = await this.loginRepository.update(login)
 
     if (successOrError.isLeft()) return left(new LoginUseCaseUpdateError(successOrError.value.message))
 
@@ -26,7 +36,7 @@ export class LoginUseCase implements LoginUseCaseContract {
   }
 
   async findAll (): Promise<Either<LoginFindAllError, unknown>> {
-    const successOrError = await this.loginService.findAll()
+    const successOrError = await this.loginRepository.findAll()
 
     if (successOrError.isLeft()) return left(new LoginUseCaseUpdateError(successOrError.value.message))
 
@@ -34,9 +44,25 @@ export class LoginUseCase implements LoginUseCaseContract {
   }
 
   async findById (id: string): Promise<Either<LoginFindByIdError, unknown>> {
-    const successOrError = await this.loginService.findById(id)
+    const successOrError = await this.loginRepository.findById(id)
 
     if (successOrError.isLeft()) return left(new LoginUseCaseFindByIdError(successOrError.value.message))
+
+    return right(successOrError.value)
+  }
+
+  async findByEmail (email: string): Promise<Either<LoginFindByEmailError, unknown>> {
+    const successOrError = await this.loginRepository.findByEmail(email)
+
+    if (successOrError.isLeft()) return left(new LoginUseCaseFindByEmailError(successOrError.value.message))
+
+    return right(successOrError.value)
+  }
+
+  async authenticationLogin (login: LoginAuthenticationInModel): Promise<Either<LoginFindByEmailError, boolean>> {
+    const successOrError = await this.loginRepository.authenticationLogin(login)
+
+    if (successOrError.isLeft()) return left(new LoginUseCaseFindByEmailError(successOrError.value.message))
 
     return right(successOrError.value)
   }
